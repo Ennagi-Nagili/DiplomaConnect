138d5cdf44e4077f72f67773bbf31f42d83df571
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2023-12-02 15:53:36 +0400

    Created popover for profile with navigation

--- src/pages/Admin/components/ProfileButton.tsx
+++ src/pages/Admin/components/ProfileButton.tsx
@@ -1,16 +1,109 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Popover from "@mui/material/Popover";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import EditIcon from "@mui/icons-material/Edit";
 
-const ProfileButton = () => {
-  const handleProfileClick = () => {
-    // Implement your logic for handling profile click
-    console.log("Profile clicked");
+const ProfileButton: React.FC = () => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+  const navigate = useNavigate();
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
+  // Responsible for closing the popover
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleEditProfileClick = () => {
+    // Navigate to the edit profile page
+    navigate("/edit-profile");
+    handleClose();
+  };
+
+  const handleProfileSettingsClick = () => {
+    // Navigate to the profile settings page or handle other actions
+    navigate("/profile-settings");
+    handleClose();
+  };
+
+  const handleLogoutClick = () => {
+    // Handle logout or other actions
+    // For demonstration purposes, navigate to the home page ("/")
+    navigate("/");
+    handleClose();
+  };
+
+  const open = Boolean(anchorEl);
+
   return (
-    <IconButton color="inherit" onClick={handleProfileClick}>
-      <AccountCircleIcon color="primary" />
-    </IconButton>
+    <div>
+      <IconButton onClick={handleClick} size="small">
+        <AccountCircleIcon />
+      </IconButton>
+
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <List sx={{ p: 2, minWidth: "200px" }}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar alt="Profile Photo" src="/path/to/profile-photo.jpg" />
+            </ListItemAvatar>
+            <ListItemText primary="John Doe" secondary="john.doe@example.com" />
+          </ListItem>
+          <Divider />
+          <ListItem
+            sx={{ alignItems: "center", cursor: "pointer" }}
+            button
+            onClick={handleEditProfileClick}
+          >
+            <EditIcon />
+            <ListItemText primary="Edit Profile" sx={{ marginLeft: "8px" }} />
+          </ListItem>
+          <ListItem
+            sx={{ alignItems: "center", cursor: "pointer" }}
+            button
+            onClick={handleProfileSettingsClick}
+          >
+            <SettingsIcon />
+            <ListItemText
+              primary="Profile Settings"
+              sx={{ marginLeft: "8px" }}
+            />
+          </ListItem>
+          <ListItem
+            sx={{ alignItems: "center", cursor: "pointer" }}
+            button
+            onClick={handleLogoutClick}
+          >
+            <ExitToAppIcon />
+            <ListItemText primary="Logout" sx={{ marginLeft: "8px" }} />
+          </ListItem>
+        </List>
+      </Popover>
+    </div>
   );
 };
 
