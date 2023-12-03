2da556dcbfa9a782515d80ce7c3de20aa7a54792
turachuseynli <turachuseynli304@gmail.com>
2023-12-03 19:06:06 +0400

    Login page

---
+++ src/Login/Login.tsx
@@ -0,0 +1,142 @@
+import * as React from 'react';
+import Logo from '/src/assets/diplomalogo.png';
+import './Login.scss'
+import { FormControl, Button, CssBaseline, TextField, FormControlLabel,Checkbox,Link,Paper,Box,Grid,Typography,createTheme, ThemeProvider  } from '@mui/material';
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit">
+        Turac
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function LogIn() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  const [red1, setRed1] = React.useState("block")
+  const [red2, setRed2] = React.useState("block")
+  const [mail, setMail] = React.useState("");
+  const [password, setPassword] = React.useState("")
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(/src/assets/student.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <img src={Logo}
+              height={100}
+              width={190}
+            />
+            <Typography component="h1" variant="h5">
+              Log In
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <FormControl required={true} fullWidth={true} >
+                <TextField
+                  margin="normal"
+                  required={true}
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={(e) => {
+                    setMail(e.target.value)
+                    if(e.target.value != "") {
+                      setRed1("none")
+                    } else {
+                      setRed1("block")
+                    }
+                  }}
+                  value={mail}
+                />
+                 <Typography className='redp' sx={{display: red1}}>
+              *Please write your E-mail
+            </Typography>
+
+              </FormControl>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                  if(e.target.value != "") {
+                    setRed2("none")
+                  } else {
+                    setRed2("block")
+                  }
+                }}
+                value={password}
+              />
+              <Typography  className="redp" sx={{display: red2}}>
+              *Please write your password
+            </Typography>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => {
+                  if(password != null && mail != "") {
+                  }
+                }}
+              >
+                Log In
+              </Button>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
