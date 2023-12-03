bfb77b714c919b12d974be2eb6b08acbfa1a90fc
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2023-12-03 12:55:59 +0400

    Basic validations

--- src/pages/Admin/pages/AddTeacher/AddTeacher.tsx
+++ src/pages/Admin/pages/AddTeacher/AddTeacher.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -15,6 +15,24 @@ import PersonIcon from "@mui/icons-material/Person";
 
 const AddTeacher: React.FC = () => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+
+  useEffect(() => {
+    setPasswordError(password.trim() === "");
+  }, [password]);
+
+  useEffect(() => {
+    setConfirmPasswordError(confirmPassword !== password);
+  }, [confirmPassword, password]);
 
   const handleTogglePasswordVisibility = () => {
     setIsPasswordVisible((prev) => !prev);
@@ -34,6 +52,49 @@ const AddTeacher: React.FC = () => {
       // Implement logic to handle the selected file
       console.log("Selected file:", selectedFile);
     }
+  };
+
+  const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+    setFirstNameError(e.target.value.trim() === "");
+  };
+
+  const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
+    setLastNameError(e.target.value.trim() === "");
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setEmailError(!/^\S+@\S+\.\S+$/.test(e.target.value));
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Add validation logic
+    if (
+      firstName.trim() === "" ||
+      lastName.trim() === "" ||
+      !/^\S+@\S+\.\S+$/.test(email) ||
+      password.trim() === "" ||
+      confirmPassword !== password
+    ) {
+      // Validation failed
+      console.log("Validation failed. Please check the form fields.");
+      return;
+    }
+
+    // Validation passed, proceed with form submission logic
+    console.log("Form submitted successfully!");
   };
 
   return (
@@ -131,7 +192,7 @@ const AddTeacher: React.FC = () => {
             </label>
           </Box>
 
-          {/* Box 2: Full Name, Email, Password */}
+          {/* Box 2: First Name, Last Name, Email, Password, Confirm Password */}
           <Box
             style={{
               display: "flex",
@@ -142,14 +203,41 @@ const AddTeacher: React.FC = () => {
               margin: "auto",
             }}
           >
-            {/* Full Name */}
-            <TextField
-              label="Full Name"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              required
-            />
+            {/* Box for First Name and Last Name with wrap */}
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "20px",
+                flexWrap: "wrap",
+              }}
+            >
+              {/* First Name */}
+              <TextField
+                label="First Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required
+                value={firstName}
+                onChange={handleFirstNameChange}
+                error={firstNameError}
+                helperText={firstNameError ? "First Name is required" : ""}
+              />
+
+              {/* Last Name */}
+              <TextField
+                label="Last Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required
+                value={lastName}
+                onChange={handleLastNameChange}
+                error={lastNameError}
+                helperText={lastNameError ? "Last Name is required" : ""}
+              />
+            </Box>
 
             {/* Email Address */}
             <TextField
@@ -159,6 +247,10 @@ const AddTeacher: React.FC = () => {
               margin="normal"
               type="email"
               required
+              value={email}
+              onChange={handleEmailChange}
+              error={emailError}
+              helperText={emailError ? "Enter a valid email address" : ""}
             />
 
             {/* Password */}
@@ -169,6 +261,37 @@ const AddTeacher: React.FC = () => {
               margin="normal"
               type={isPasswordVisible ? "text" : "password"}
               required
+              value={password}
+              onChange={handlePasswordChange}
+              error={passwordError}
+              helperText={passwordError ? "Password is required" : ""}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleTogglePasswordVisibility}>
+                      {isPasswordVisible ? (
+                        <VisibilityIcon />
+                      ) : (
+                        <VisibilityOffIcon />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            {/* Confirm Password */}
+            <TextField
+              label="Confirm Password"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              type={isPasswordVisible ? "text" : "password"}
+              required
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              error={confirmPasswordError}
+              helperText={confirmPasswordError ? "Passwords do not match" : ""}
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
@@ -184,9 +307,14 @@ const AddTeacher: React.FC = () => {
               }}
             />
           </Box>
-
-          {/* Add other fields as needed */}
         </CardContent>
+
+        {/* Submit Button */}
+        <Box sx={{ textAlign: "center", margin: "16px" }}>
+          <Button variant="contained" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Box>
       </Card>
     </>
   );
