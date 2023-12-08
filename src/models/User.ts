73cb8d90e952ba7bfe822302a7d16a5812e9ef62
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2023-12-08 18:49:15 +0400

    Created interfaces based on technical requirements

--- src/models/User.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    firstName: string,
-    lastName: string,
-    fatherName?: string,
-    phoneNumber?: string,
-    email: string,
-    password: string,
-}
---
+++ src/models/models.ts
@@ -0,0 +1,49 @@
+export interface User {
+  type: "admin" | "teacher" | "student";
+  profilePhoto?: File;
+  firstName: string;
+  lastName: string;
+  fatherName: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+}
+
+// const departmentOptions = [...(["Algebra and Geometry", "Analysis", "Mathematical Control Theory"] as const)];
+
+export interface Teacher extends User {
+  // TODOL This should be just a limited set of departments
+  department?: string;
+  subject?: string;
+}
+
+export interface Student extends User {
+  group: string;
+}
+
+// Students sends cover letter to teacher to ask him to be his supervisor
+export interface Application {
+  author: User; // ??
+  teacher: Teacher;
+  coverLetter: string;
+}
+
+export interface Exercise {
+  student: Student;
+  exerciseTitle: string;
+  exerciseNumber: number;
+  status: string;
+  description?: string;
+  deadline?: Date; // In the document, it is deadlines ??
+  supplementaryMaterials: string[];
+}
+
+export interface studentAnswer {
+  comment?: string;
+  materials?: string[];
+}
+
+export interface teacherResponse {
+  status: string;
+  comment?: string;
+}
