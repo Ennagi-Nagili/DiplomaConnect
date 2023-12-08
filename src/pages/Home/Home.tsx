bb6e96a8f6687c500eef4777381f5c4dca4972c8
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2023-12-08 20:03:19 +0400

    Deleting unnecessary codes for first pull request

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,61 +1,23 @@
 import "./Home.scss";
-import FormInput, { FormInputProps } from "../../components/FormInput/FormInput";
-import { Form, Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [fatherName, setFatherName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPaswords, setConfirmPassword] = useState("");
-
-  const user: User = {
-    firstName: firstName,
-    lastName: lastName,
-    fatherName: fatherName,
-    phoneNumber: phoneNumber,
-    email: email,
-    password: password,
-  }
-
-  console.log("re-rencering");
+  // Note: Turaj's authentication code should be here. For now, I placed my own code.
 
   const navigate = useNavigate();
-
-  const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // Clicking on a "Submit" button, prevent it from submitting a form
-    navigate("/profile");
-  };
-
-  // Add: id, inputType, required ???
-  const inputAttributes = [
-    { label: "First Name", setValue: setFirstName,  },
-    { label: "Last Name", setValue: setLastName, required: true },
-    { label: "Father's Name", setValue: setFatherName, required: false },
-    { label: "Phone Number", setValue: setPhoneNumber, required: false },
-    { label: "Email", setValue: setEmail, required: true },
-    { label: "Password", setValue: setPassword, required: true },
-    { label: "Confirm Password", setValue: setConfirmPassword, required: true },
-  ];
 
   return (
     <div className="home-container">
       <h1>Home Page</h1>
 
-      <form onSubmit={handleOnSubmit}>
-        <h2>Sign Up</h2>
-
-        {inputAttributes.map((attributes) => (
-          <FormInput {...attributes} />
-        ))}
-
+      <h2>Sign Up</h2>
+      <div>
         <Link to="/login">Already have an account?</Link>
-          
-        <button type="submit">Submit</button>
-      </form>
+      </div>
+
+      <button type="submit" onClick={() => navigate("/profile")}>
+        Submit
+      </button>
     </div>
   );
 };
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,18 +1,8 @@
-import { Box } from "@mui/material";
-
 const Login = () => {
   return (
-    <Box
-      component="div"
-      className="login-main"
-      sx={{ backgroundColor: "gray", height: "100vh", width: "100vw" }}
-    >
-
-      <div>
-        <h1>Login Page</h1>
-      </div>
-      
-    </Box>
+    <div>
+      <h1>Login Page</h1>
+    </div>
   );
 };
 
