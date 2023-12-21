5309ad40bce5abd69f9e9ddc138a647a26ca0136
Murad Nurmammadov <nurmammadovmurad@gmail.com>
2023-12-21 21:16:19 +0400

    Made UserGrid for teacher responsive + minor fixes

--- src/pages/Admin/pages/UsersDataGrid/UsersDataGrid.tsx
+++ src/pages/Admin/pages/UsersDataGrid/UsersDataGrid.tsx
@@ -1,11 +1,11 @@
 import './UsersDataGrid.scss';
-import { Avatar } from '@mui/material';
+import { Avatar, useMediaQuery } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { DataGrid, GridActionsCellItem, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { Student, Teacher } from '../../../../models/models';
 import { deleteUser, selectStudents, selectTeachers } from '../../../../services/reducers/users.slice';
 import { useAppDispatch, useAppSelector } from '../../../../services/hooks';
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import DataGridToolbar from '../../components/DataGridToolbar';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
@@ -16,7 +16,6 @@ export const UsersDataGrid = () => {
 
   const teachers = useAppSelector(selectTeachers);
   const students = useAppSelector(selectStudents);
-
   const users = pageMode === 'teachers' ? teachers : students;
 
   const deleteRow = useCallback(
@@ -30,8 +29,46 @@ export const UsersDataGrid = () => {
     [users],
   );
 
+  // Media queries to use in teacher DataGrid
+  const isTeacherScreen_1 = useMediaQuery('(max-width: 1255px)');
+  const isTeacherScreen_2 = useMediaQuery('(max-width: 1080px)');
+  const isTeacherScreen_3 = useMediaQuery('(max-width: 880px)');
+  const isTeacherScreen_4 = useMediaQuery('(max-width: 705px)');
+  const isTeacherScreen_5 = useMediaQuery('(max-width: 540px)');
+
   // Columns:
-  const teacherColumns: GridColDef[] = [
+  const columns: GridColDef[] = [
+    // Profile Photo
+    {
+      field: 'avatar',
+      headerName: '',
+      width: 30,
+      sortable: false,
+      headerAlign: 'center',
+      align: 'center',
+      renderCell: (params) => {
+        console.log('rendering cell');
+        return <Avatar src={params.row.profilePhoto} style={{ height: '34px', width: '34px' }} />;
+      },
+    },
+    // ID
+    {
+      field: 'id',
+      headerName: 'ID',
+      width: 30,
+      headerAlign: 'center',
+      align: 'center',
+    },
+    // Full Name
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: true,
+      width: isTeacherScreen_5 ? 170 : 200,
+      valueGetter: (params: GridValueGetterParams) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+    // Teacher Specific:
     {
       field: 'department',
       headerName: 'Department',
@@ -42,85 +79,75 @@ export const UsersDataGrid = () => {
       headerName: 'Subject',
       width: 140,
     },
-  ];
-
-  const studentColumns: GridColDef[] = [
+    // Student Specific:
     {
       field: 'group',
       headerName: 'Group',
+      width: 80,
+    },
+    // Email
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 160,
+    },
+    // Phone Number
+    {
+      field: 'phoneNumber',
+      headerName: 'Phone Number',
       width: 140,
     },
-  ];
-
-  const columns = useMemo<GridColDef[]>(
-    () => [
-      {
-        field: 'avatar',
-        headerName: '',
-        width: 30,
-        sortable: false,
-        headerAlign: 'center',
-        align: 'center',
-        renderCell: (params) => {
-          console.log('rendering cell');
-          return <Avatar src={params.row.profilePhoto} style={{ height: '34px', width: '34px' }} />;
-        },
-      },
-      {
-        field: 'id',
-        headerName: 'ID',
-        width: 30,
-        headerAlign: 'center',
-        align: 'center',
-      },
-      {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: true,
-        width: 200,
-        valueGetter: (params: GridValueGetterParams) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-      },
-      // Spread teacher or student specific column information
-      ...(pageMode === 'teachers' ? teacherColumns : studentColumns),
-      {
-        field: 'email',
-        headerName: 'Email',
-        width: 140,
-      },
-      {
-        field: 'phoneNumber',
-        headerName: 'Phone Number',
-        width: 140,
-      },
-      {
-        field: 'actions',
-        type: 'actions',
-        headerName: 'Actions',
-        width: 100,
-        headerAlign: 'center',
-        align: 'center',
-        cellClassName: 'actions',
-        getActions: (params) => [
-          // Redirect to user
-          <>
-            {/* // Redirect to user */}
+    // Actions
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: pageMode === 'teachers' ? 100 : 75,
+      headerAlign: 'center',
+      align: 'center',
+      cellClassName: 'actions',
+      getActions: (params) => [
+        // Redirect to user
+        <>
+          {/* // Redirect to user */}
+          {pageMode === 'teachers' && (
             <Link to={`/admin/teachers/${params.id}`}>
               <GridActionsCellItem key="redirectToUser" icon={<OpenInNewIcon />} label="Redirect" />
             </Link>
-            {/* // Delete User */}
-            <GridActionsCellItem key="deleteUser" icon={<DeleteIcon />} label="Delete" onClick={deleteRow(params.row)} />
-          </>,
-        ],
-      },
-    ],
-    [deleteRow, pageMode],
-  );
+          )}
+          {/* // Delete User */}
+          <GridActionsCellItem key="deleteUser" icon={<DeleteIcon />} label="Delete" onClick={deleteRow(params.row)} />
+        </>,
+      ],
+    },
+  ];
+
+  let visibleColumns: GridColDef[] = columns;
+  if (pageMode === 'teachers') {
+    visibleColumns = visibleColumns.filter((column) => column.field !== 'group');
+    if (isTeacherScreen_1) {
+      visibleColumns = visibleColumns.filter((column) => column.field !== 'phoneNumber');
+    }
+    if (isTeacherScreen_2) {
+      visibleColumns = visibleColumns.filter((column) => column.field !== 'email');
+    }
+    if (isTeacherScreen_3) {
+      visibleColumns = visibleColumns.filter((column) => column.field !== 'subject');
+    }
+    if (isTeacherScreen_4) {
+      visibleColumns = visibleColumns.filter((column) => column.field !== 'department');
+    }
+    if (isTeacherScreen_5) {
+      visibleColumns = visibleColumns.filter((column) => column.field !== 'avatar');
+    }
+  } else if (pageMode === 'students') {
+    visibleColumns = visibleColumns.filter((column) => !['department', 'subject'].includes(column.field));
+  }
 
   return (
     <DataGrid
       rows={users} // teachers are initial state, rows are secondary
-      columns={columns}
+      columns={visibleColumns}
       initialState={{
         pagination: { paginationModel: { pageSize: 10 } },
       }}
@@ -132,7 +159,7 @@ export const UsersDataGrid = () => {
       sx={{
         minWidth: '330px',
         width: '80%',
-        maxWidth: '1000px',
+        maxWidth: pageMode === 'teachers' ? '1000px' : '800px',
         margin: 'auto',
       }}
       autoHeight
@@ -141,5 +168,3 @@ export const UsersDataGrid = () => {
     />
   );
 };
-
-// export default UsersDataGrid;
